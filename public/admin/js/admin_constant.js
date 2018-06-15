4d97b7e9e1b4a66e6ac9648056f94f0801033b69
Martin SangDo <martin.sangdo@gmail.com>
2018-06-15 13:45:14 +0700

    + add init files (home)

--- public/admin/js/admin_constant.js
+++
@@ -1,7 +0,0 @@
-//author: Martin SangDo
-
-var STR_MESS = {
-    MISSING_INPUT: 'Missing values of required fields',
-    WRONG_CAPTCHA: 'Invalid captcha',
-    INVALID_LOGIN_ADMIN: 'Username or password is not correct'
-};
