543b585129207b3b91f035b1efc677f072015196
Martin SangDo <martin.sangdo@gmail.com>
2018-07-10 23:39:43 +0700

    Add publication

---
+++ application/views/front/webview/xufa/publication.php
@@ -0,0 +1,130 @@
+<!-- Publications Panel -->
+<div class="card border-0 g-mb-40">
+    <!-- Panel Header -->
+    <div class="card-header d-flex align-items-center justify-content-between g-bg-gray-light-v5 border-0 g-mb-15">
+        <h3 class="h6 mb-0">
+            <i class="icon-layers g-pos-rel g-top-1 g-mr-5"></i> PUBLICATIONS
+        </h3>
+    </div>
+    <!-- End Panel Header -->
+
+    <!-- Panel Body -->
+    <div class="js-scrollbar card-block u-info-v1-1 g-bg-white-gradient-v1--after g-height-300 g-pa-0">
+        <ul class="list-unstyled">
+            <li class="media g-brd-around g-brd-gray-light-v4 g-brd-left-3 g-brd-black-left rounded g-pa-20 g-mb-10">
+                <div class="media-body">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="h6 g-font-weight-600 g-color-black"><a target="_blank" href="https://link.springer.com/chapter/10.1007/978-3-642-34487-9_1">1) Centroid Neural Network with Simulated Annealing and Its Application to Color Image Segmentation</a></h5>
+                        <span class="small text-nowrap g-color-blue">Nov. 2012</span>
+                    </div>
+                    <i>Lecture Notes in Computer Science (LNCS), Volume 7665, pp 1-8</i>
+                    <p>Centroid Neural Network (CNN) with simulated annealing is proposed and applied to a color image segmentation problem in this paper. CNN is essentially an unsupervised competitive neural network scheme and is a crucial algorithm to diminish the empirical process of parameter adjustment required in many unsupervised competitive learning algorithms including Self-Organizing Map...</p>
+                    <div class="d-block">
+                                              <span class="g-color-cyan g-font-size-default g-mr-3">
+                                                <i class="icon-user"></i>
+                                              </span>
+                        <em class="g-color-gray-dark-v4 g-font-style-normal g-font-size-default">Do-Thanh Sang, Dong-Min Woo, Dong-Chul Park</em>
+                    </div>
+                </div>
+            </li>
+            <li class="media g-brd-around g-brd-gray-light-v4 g-brd-left-3 g-brd-black-left rounded g-pa-20 g-mb-10">
+                <div class="media-body">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="h6 g-font-weight-600 g-color-black"><a target="_blank" href="https://link.springer.com/chapter/10.1007/978-3-642-33478-8_73">2) Color Image Segmentation Using Centroid Neural Network</a></h5>
+                        <span class="small text-nowrap g-color-blue">Oct. 2012</span>
+                    </div>
+                    <i>Lecture Notes in Computer Science (LNCS), Volume 7530, pp 588-595</i>
+                    <p>Color image segmentation has been attracting more and more attention, mainly because color images can provide more information than gray level images. Many methods have been proposed so far to deal with the problem. However, most methods require fine tuning of parameters, which can be attained after repetitive trial and error. This paper discusses unsupervised learning in terms of Centroid Neural Network (CNN)...</p>
+                    <div class="d-block">
+                                              <span class="g-color-cyan g-font-size-default g-mr-3">
+                                                <i class="icon-user"></i>
+                                              </span>
+                        <em class="g-color-gray-dark-v4 g-font-style-normal g-font-size-default">Do-Thanh Sang, Dong-Min Woo, Dong-Chul Park</em>
+                    </div>
+                </div>
+            </li>
+            <li class="media g-brd-around g-brd-gray-light-v4 g-brd-left-3 g-brd-black-left rounded g-pa-20 g-mb-10">
+                <div class="media-body">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="h6 g-font-weight-600 g-color-black"><a target="_blank" href="https://link.springer.com/chapter/10.1007/978-3-642-16773-7_35">3) Financial Time Series Prediction in Cooperating with Event Knowledge : A Fuzzy Approach</a></h5>
+                        <span class="small text-nowrap g-color-blue">Nov. 2010</span>
+                    </div>
+                    <i>Lecture Notes in Computer Science (LNCS), Volume 6438, Springer-Verlag Berlin, pp 406-417</i>
+                    <p>A number of researchers have used historical numeric time series data to forecast financial markets, i.e. stock prices, and they achieved some results with reasonable accuracies. However, there are various non-numerical factors that influence prices such as company's performance, government involvement, trends of the market, changes in economic activity and so forth. We attempt to take such factors into account to our recent study....</p>
+                    <div class="d-block">
+                                              <span class="g-color-cyan g-font-size-default g-mr-3">
+                                                <i class="icon-user"></i>
+                                              </span>
+                        <em class="g-color-gray-dark-v4 g-font-style-normal g-font-size-default">Do-Thanh Sang, Dong-Min Woo, Dong-Chul Park</em>
+                    </div>
+                </div>
+            </li>
+            <li class="media g-brd-around g-brd-gray-light-v4 g-brd-left-3 g-brd-black-left rounded g-pa-20 g-mb-10">
+                <div class="media-body">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="h6 g-font-weight-600 g-color-black"><a target="_blank" href="https://ieeexplore.ieee.org/document/5615505">4) Standard Additive Model in Data Mining</a></h5>
+                        <span class="small text-nowrap g-color-blue">Oct. 2010</span>
+                    </div>
+                    <i>International Conference on Cyber-enabled Distributed Computing and Knowledge Discovery (CyberC)</i>
+                    <p>The habitual purpose of data mining is prediction, one of the most direct real-world applications. There are many technologies available to data mining in literature and they achieved some results with reasonable accuracies. This paper designs and implements an advanced model based on fuzzy inference system, namely Standard Additive Model (SAM) for forecasting the output of any record given the input variables only from the database, the age of abalone in particular...</p>
+                    <div class="d-block">
+                                              <span class="g-color-cyan g-font-size-default g-mr-3">
+                                                <i class="icon-user"></i>
+                                              </span>
+                        <em class="g-color-gray-dark-v4 g-font-style-normal g-font-size-default">Do-Thanh Sang, Dong-Min Woo, Dong-Chul Park</em>
+                    </div>
+                </div>
+            </li>
+            <li class="media g-brd-around g-brd-gray-light-v4 g-brd-left-3 g-brd-black-left rounded g-pa-20 g-mb-10">
+                <div class="media-body">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="h6 g-font-weight-600 g-color-black"><a target="_blank" href="https://ieeexplore.ieee.org/abstract/document/5480374">5) Intensifying the Performance of Nonlinearity Approximation by an Optimal Fuzzy System</a></h5>
+                        <span class="small text-nowrap g-color-blue">Apr. 2010</span>
+                    </div>
+                    <i>International Conference on Information Science and Applications (ICISA)</i>
+                    <p>A technique to optimize the Standard Additive Model (SAM) fuzzy system for nonlinear system approximation is presented. First, fuzzy rules are initialized more much than usual by employing Centroid Neural Network (CNN) and then the genetic algorithm-based optimization process used to exclude unnecessary and redundant rules; thereafter, the fuzzy rule parameters are tuned by the gradient descent method incorporated with momentum technique...</p>
+                    <div class="d-block">
+                                              <span class="g-color-cyan g-font-size-default g-mr-3">
+                                                <i class="icon-user"></i>
+                                              </span>
+                        <em class="g-color-gray-dark-v4 g-font-style-normal g-font-size-default">Do-Thanh Sang, Ho-Hai Nguyen, Dong-Min Woo, Seung-Soo Han, Dong-Chul Park</em>
+                    </div>
+                </div>
+            </li>
+            <li class="media g-brd-around g-brd-gray-light-v4 g-brd-left-3 g-brd-black-left rounded g-pa-20 g-mb-10">
+                <div class="media-body">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="h6 g-font-weight-600 g-color-black"><a target="_blank" href="https://link.springer.com/chapter/10.1007/978-3-642-12101-2_29">6) Standard Additive Fuzzy System for Stock Price Forecasting</a></h5>
+                        <span class="small text-nowrap g-color-blue">Mar. 2010</span>
+                    </div>
+                    <i>Lecture Notes in Computer Science (LNCS), Volume 5991, pp 279-288</i>
+                    <p>Stock price forecasting has attracted tremendous attention of researchers over the past several decades. Many techniques thus have been proposed so far to deal with the problem. This paper presents an application of a computational intelligence technique - a fuzzy inference system, namely Standard Additive Model (SAM), for predicting stock price time series data. The modelling and learning power of the SAM have been benefited to build the model ...</p>
+                    <div class="d-block">
+                                              <span class="g-color-cyan g-font-size-default g-mr-3">
+                                                <i class="icon-user"></i>
+                                              </span>
+                        <em class="g-color-gray-dark-v4 g-font-style-normal g-font-size-default">Do-Thanh Sang, Thi Thanh Nguyen, Dong-Min Woo, Dong-Chul Park</em>
+                    </div>
+                </div>
+            </li>
+            <li class="media g-brd-around g-brd-gray-light-v4 g-brd-left-3 g-brd-black-left rounded g-pa-20 g-mb-10">
+                <div class="media-body">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="h6 g-font-weight-600 g-color-black">3D Line Extraction Using Digital Elevation Map</h5>
+                        <span class="small text-nowrap g-color-blue">May. 2011</span>
+                    </div>
+                    <i>Master Thesis</i>
+                    <p>The task of extracting and discovering the geometric feature such as points, lines, corners and curves plays an important role in object recognition, 3D modeling, robot mapping and navigation. In this paper, we present an alternative 3D line extraction method by using Digital Elevation Map (DEM) in cooperated with self-consistency principal. DEM is first revised by self-consistency process which only remains good elevation data...</p>
+                    <div class="d-block">
+                                              <span class="g-color-cyan g-font-size-default g-mr-3">
+                                                <i class="icon-user"></i>
+                                              </span>
+                        <em class="g-color-gray-dark-v4 g-font-style-normal g-font-size-default">Do-Thanh Sang, Dong-Min Woo</em>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <!-- End Panel Body -->
+</div>
+<!-- End Publications Panel -->
